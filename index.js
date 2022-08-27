738edffd3b2923ee25a14e1fa596ad4a81fd54c9
CorrineMuriuki <Corrine.Muriuki@student.moringaschool.com>
2022-08-28 01:56:34 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 function addingEventListener() {
+    //set a variable to refer to a specific ID button
+const button = document.getElementById('button');
+
+//Once we have the button we pass the function, i passed an anonymous function
+button.addEventListener('click', function() {
+console.log('click');
+});
 }
